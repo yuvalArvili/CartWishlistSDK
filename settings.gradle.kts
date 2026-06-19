318ad2ddfcb0119446f9d24d9285dedcec413a36
yuvalArvili <yuvalar@omnitelecom.co.il>
2026-06-19 19:56:10 +0300

    chore: Gradle multi-module project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "shopping_cart_sdk"
+include(":app")
+include(":cartwishlist")
+ 
